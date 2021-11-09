d49fccd1ff1ba9707d873d9872f2eed1b7db5814
A.J. Romaniello <ajiellodev@gmail.com>
2021-11-08 22:53:57 -0700

    Completed.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function bfs(rootNode, vertices, edges){
+    rootNode.distance = 0
+    let found = [rootNode]
+    let final = [rootNode]
+    while (found.length > 0) {
+        let cur = found.shift()
+        let adjacent = findAdjacent(cur.name, vertices, edges)
+        found = found.concat(adjacent)
+        markDistanceAndPredecessor(cur, adjacent)
+        final = final.concat(adjacent)
+    }
 
+    return final
+}
+
+function findAdjacent(address, vertices, edges) {
+    let filtered = edges.filter( edge => { return edge.includes(address) })
+    let flattened = filtered.flat().filter( street => { return street !== address })
+    return vertices.filter( vert => { return flattened.includes(vert.name) && vert.distance === null })
+}
+
+function markDistanceAndPredecessor(start, adjacent) {
+    adjacent.forEach( e => {
+        e.distance = start.distance + 1
+        e.predecessor = start
+    })
 }
